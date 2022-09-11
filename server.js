dd73f7f4033733f74dd3401479c54aaa4b2520fe
mvpc-11232 <mvp2612@hotmail.com>
2022-09-11 23:09:09 +0530

    Auto Push Commit

--- server.js
+++ server.js
@@ -268,7 +268,10 @@ function mongoConnect() {
     var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
     var client = mongoClient.connect(mongoURL,
       {
-        tlsCAFile: `/home/roboshop/rds-combined-ca-bundle.pem` 
+        // Mutable & Immutable
+        //tlsCAFile: `/home/roboshop/user/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+        // Container
+        tlsCAFile: `/home/roboshop/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
     }, (error, client) => {
     if(error) {
         reject(error);
